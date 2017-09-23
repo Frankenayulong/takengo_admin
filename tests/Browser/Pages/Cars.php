f7db249ac1ee39a126d8b4a0cd73cc5b6b1d5c56
= <s3642807@student.rmit.edu.au>
2017-09-23 20:51:43 +1000

    add dusk test case

--- tests/Browser/Pages/Cars.php
+++ tests/Browser/Pages/Cars.php
@@ -43,14 +43,46 @@ class Cars extends BasePage
         $browser->clickLink('1');
         $browser->visit('/orders');
         $browser->assertSee('Orders');
+        $browser->assertSee('Car Name');
+        $browser->assertSee('Cust. Name');
+        $browser->assertSee('Cust. Email');
+        $browser->assertSee('Date');
+        $browser->assertSee('Start Time');
+        $browser->assertSee('End Time');
+        $browser->assertSee('Order Time');
+        $browser->assertSee('Duration');
+        $browser->assertSee('Price');
+        $browser->assertSee('Status');
         $browser->visit('/messages');
         $browser->assertSee('Messages');
+        $browser->assertSee('Name');
+        $browser->assertSee('Email');
+        $browser->assertSee('Phone');
+        $browser->assertSee('Date');
+        $browser->assertSee('Content');
+        $browser->assertSee('Status');
         $browser->visit('/newsletters');
         $browser->assertSee('Newsletter');
+        $browser->assertSee('Email');
+        $browser->assertSee('Registered At');
         $browser->visit('/users');
         $browser->assertSee('Takengo Users');
+        $browser->assertSee('First Name');
+        $browser->assertSee('Last Name');
+        $browser->assertSee('Email');
+        $browser->assertSee('Phone');
+        $browser->assertSee('Birth Date');
+        $browser->assertSee('Driver License Number');
+        $browser->assertSee('Total Order');
+        $browser->assertSee('Registered On');
         $browser->visit('/admins');
         $browser->assertSee('Takengo Admins');
+        $browser->assertSee('First Name');
+        $browser->assertSee('Last Name');
+        $browser->assertSee('Email');
+        $browser->assertSee('Phone');
+        $browser->assertSee('Last IP');
+        $browser->assertSee('Created At');
     }
 
     /**
